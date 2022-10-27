5a0393683fa4d1c7cc5cd892ea8b88ef8e7d9b49
MATTHEW STEVEN CRUMP <u0894108@lab1-2.eng.utah.edu>
2022-10-26 20:26:46 -0600

    Added beginnings of test bench

--- skywater_template/projects/OneBitProcessor_tb.v
+++ skywater_template/projects/OneBitProcessor_tb.v
@@ -1,9 +1,9 @@
 /*
 Matthew Crump
 ECE5710, Fall 2022
-The Stupid MAchine, a 1-Bit Processor
+The Stupid Machine, a 1-Bit Processor
 Test Bench
-Tests for the OneBitProcessor and associated sun-modules
+Tests for the OneBitProcessor and associated sub-modules
 */
 `timescale 1 ns / 100 ps
 
@@ -11,10 +11,23 @@ module OneBitProcessor_tb;
 	// inputs to the DUT
 	reg clk;
 	reg reset;
+	reg enable;
 	reg [1:0] input_signals;
 	// outputs from the DUT
 	wire [6:0] regs_out;
 
+	OneBitProcessor dut (
+		.clk(clk),
+		.reset(reset),
+		.en(enable),
+		.inReg(input_signals),
+		.outReg(regs_out));
+
+	initial begin
+		$display($time, " << Starting the Simulation");
+		clk = 0;
+		
+	end
 
 endmodule
 	
